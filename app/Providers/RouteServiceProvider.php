014b4cf6d7d4f18f57ea6a0637cb45eec6e39d11
ITスクール金沢西 <itschoolkanazawawest@aitisukurukanazawanoMacBook-Pro.local>
2024-04-05 16:43:13 +0900

    routes-controller_edit

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -18,6 +18,8 @@ class RouteServiceProvider extends ServiceProvider
      * @var string
      */
     public const HOME = '/dashboard';
+    public const OWNER_HOME = '/owner/dashboard';
+    public const ADMIN_HOME = '/admin/dashboard';
 
     /**
      * Define your route model bindings, pattern filters, and other route configuration.
@@ -35,6 +37,21 @@ class RouteServiceProvider extends ServiceProvider
 
             Route::middleware('web')
                 ->group(base_path('routes/web.php'));
+
+            Route::prefix('admin')
+            ->as('admin.')
+            ->middleware('web')
+            ->group(base_path('routes/admin.php'));
+
+            Route::prefix('owner')
+            ->as('owner.')
+            ->middleware('web')
+            ->group(base_path('routes/owner.php'));
+
+            Route::prefix('/')
+            ->as('user.')
+            ->middleware('web')
+            ->group(base_path('routes/web.php'));
         });
     }
 }
